c175b0d0bf3ca605acf491bda8b3bb7d6747f0d0
Vika <70843852+inomeri@users.noreply.github.com>
2020-09-29 21:10:52 +0300

    Зайти на Кинопоиск, найти 5 любимых фильмов.
    Табличка films:
        title - название фильма (текст)
        id (число) соответствует film_id в табличке persons2content
        country страна (тест)
        box_office сборы в долларах (число)
        release_date дата выпуска (date)
    
    Табличка persons (актёры, режиссёры и т.д.)
        id (число) - соответствует person_id в табличке persons2content
        fio (текст) фамилия, имя
    
    Табличка persons2content
        person_id (число) - id персоны
        film_id (число) - id контента
        person_type (текст) тип персоны (актёр, режиссёр и т.д.)
    
    Реализуется схема БД "Звезда" с центром в табличке persons2content

---
+++ 1_films2content.sql
@@ -0,0 +1,39 @@
+create table persons (
+  id serial not null primary key,
+  fio varchar (128) not null
+);
+
+create table films (
+  title varchar(128) not null,
+  id serial not null primary key,
+  country varchar(64) not null,
+  box_office bigint not null,
+  release_date date not null
+);
+
+create table persons2content (
+  person_id int REFERENCES persons (id),
+  film_id int REFERENCES films (id),
+  person_type varchar(128) not null
+);
+
+insert into films values 
+  ('Pulp Fiction', 1, 'USA', 213928762, '1994-05-21'),
+  ('Lock, Stock and Two Smoking Barrels', 2, 'Great Britain', 3753929, '1998-08-23'),
+  ('Hauru no ugoku shiro', 3, 'Japan', 235347345, '2005-09-05'),
+  ('Blade Runner', 4, 'USA', 27615743, '1982-06-25'),
+  ('Star Wars: Episode VII - The Force Awakens', 5, 'USA', 2068223624, '2015-12-14');
+
+insert into persons values
+  (1, 'John Travolta'), (2, 'Guy Ritchie'), (3, 'Joe Hisaishi'), (4, 'Ridley Scott'), (5, 'Rick Carter');
+
+insert into persons2content values
+  (5, 5, 'artist'),
+  (4, 4, 'producer'),
+  (3, 3, 'musician'),
+  (2, 2, 'screenwriter'),
+  (1, 1, 'actor');
+  
+select * from films;
+select * from persons;
+select * from persons2content;
