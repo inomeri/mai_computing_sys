493ecd21538492e155a1f42f1aff30d2d6038dfc
Vika <70843852+inomeri@users.noreply.github.com>
2020-12-03 23:42:43 +0300

    Update agg.js

--- agg.js
+++ agg.js
@@ -7,19 +7,19 @@ print('Tags count: ', db.tags.find().count());
 /*
         2. Добавляем фильтрацию: считаем только количество фильмов с тегом Adventure
 */
-print('"Adventure" tags count: ', db.tags.find({"tag_name" : "Adventure"}).count());
+print('"Adventure" tags count: ', db.tags.find({tag_name : "Adventure"}).count());
 /*
         3. Очень сложный запрос: используем группировку данных посчитать количество вхождений для каждого тега
         и напечатать top-3 самых популярных
 */
 printjson(
         db.tags.aggregate([
-                {"$group": {
-                                "_id": "$tag_name",
-                                "tag_count": { "$sum": 1 }
+                { $group: {
+                                _id: $tag_name,
+                                tag_count: { $sum: 1 }
                            }
                 },
-                {"$sort": {"tag_count" : -1 }},
-                {"limit" 3}
+                { $sort: { tag_count : -1 }},
+                { limit 3}
         ])['_batch']
 );
